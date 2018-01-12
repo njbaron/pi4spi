65bbb917fb28197051dbb14da6f83c32e7636bc6
Nicholas Baron <njbaron@users.noreply.github.com>
2018-01-11 22:45:49 -0700

    yuo

--- src/main/java/com/devknight/pi4spi/executable.java
+++ src/main/java/com/devknight/pi4spi/executable.java
@@ -9,17 +9,16 @@ public class executable {
     public static void main(String args[]) {
         System.out.println("Welcome to JavaSPI");
         mcp3008 adc = new mcp3008(18,23,24,25);
-
+        ArrayList<Double> voltages = new ArrayList<Double>();
 
         try {
             while (true) {
-                ArrayList<Double> voltages = new ArrayList<Double>();
                 for (int i = 0; i < 8; i++) {
                     voltages.add(adc.getVoltage(i));
                 }
                 String s = voltages.toString();
                 System.out.println(s);
-
+                voltages.clear();
             }
         }
         catch(Exception e) {
